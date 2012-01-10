9e8b2f0d19d18670b112d27b427ef2923b257c6f
Toru Inoko <toru0707@gmail.com>
2012-01-11 00:08:11 +0900

    Initial Commit

---
+++ meeetletphonegap/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  meeetletphonegap
+//
+//  Created by 猪子 徹 on 12/01/11.
+//  Copyright SCS 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
